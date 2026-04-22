8f860795d8b1aefd90414368217d836d84b9b866
Pratik Shinde <pratikshinde9957@gmail.com>
2026-04-22 18:36:40 +0530

    support css

--- hireflow/components/popup/SupportChat.jsx
+++ hireflow/components/popup/SupportChat.jsx
@@ -73,7 +73,7 @@ function SupportChat() {
         <>
             <button
                 onClick={() => setIsOpen(!isOpen)}
-                className="fixed right-5 bottom-10 sm:bottom-10 top-5 sm:top-auto"
+                className="fixed right-5 bottom-10 sm:bottom-10 top-5 sm:top-10"
             >
                 <SupportAgentIcon height={24} width={24} />
             </button>
